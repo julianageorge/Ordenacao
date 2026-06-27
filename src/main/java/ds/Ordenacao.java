421e840a8826433bfd999acfdd414cfdb5ec5332
juliana <121284395+julianageorge@users.noreply.github.com>
2026-06-27 20:10:23 +0300

    Update Ordenacao.java with new method to test incremental mode

--- src/main/java/ds/Ordenacao.java
+++ src/main/java/ds/Ordenacao.java
@@ -1,15 +1,27 @@
 package ds;
+
 public class Ordenacao {
-  public static void ordena (int v[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-      int min = i;
-      for (int j = i + 1; j < n; j++)
-        if (v[j] < v[min]) 
-          min = j;
-      /* @{\it Troca v[min] e v[i]}@ */
-      int x = v[min]; 
-      v[min] = v[i]; 
-      v[i] = x;
+
+    public static void ordena(int v[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++)
+                if (v[j] < v[min])
+                    min = j;
+
+            int x = v[min];
+            v[min] = v[i];
+            v[i] = x;
+        }
     }
-  }
+
+    // New method
+    public static boolean isSorted(int[] v, int n) {
+        for (int i = 1; i < n; i++) {
+            if (v[i - 1] > v[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
